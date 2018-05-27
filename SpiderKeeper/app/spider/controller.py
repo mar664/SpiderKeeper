8454620f4e3202958d17af81b78eb2dfa4a97708
Matthew Ruse <matthew.ruse@gmail.com>
2018-05-27 21:03:53 +1000

    Update controller.py

--- SpiderKeeper/app/spider/controller.py
+++ SpiderKeeper/app/spider/controller.py
@@ -483,10 +483,10 @@ def utility_processor():
         if not total_seconds:
             return '-'
         if total_seconds < 60:
-            return '%s s' % total_seconds
+            return '%d s' % total_seconds
         if total_seconds < 3600:
-            return '%s m %s s' % (int(total_seconds / 60), int(total_seconds % 60))
-        return '%s h %s m %s s' % (int(total_seconds / 3600), int((total_seconds % 3600) / 60), int((total_seconds % 3600) % 60))
+            return '%d m %d s' % (int(total_seconds / 60), int(total_seconds % 60))
+        return '%d h %d m %d s' % (int(total_seconds / 3600), int((total_seconds % 3600) / 60), int((total_seconds % 3600) % 60))
     
     def datetimefilter(value):
         local_dt = utc.localize(value).astimezone(timezone("Australia/Sydney"))
